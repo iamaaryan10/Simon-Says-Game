67d4805c9fbe669468f01cafda2ea802429ce2a7
Aaryan Shrivastava <136842195+iamaaryan10@users.noreply.github.com>
2025-12-05 15:03:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ document.addEventListener("keypress", function () {
 
 function btnFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function () {
+    setTimeout(() => {
         btn.classList.remove("flash");
-    }, 1000); // blink for 1 second
+    }, 400); // matches smooth animation time
 }
 
 function levelUp() {
@@ -28,7 +28,7 @@ function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
 
-    let randIdx = Math.floor(Math.random() * 4); // 0–3 for 4 colors
+    let randIdx = Math.floor(Math.random() * 4); // 4 buttons
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`);
 
@@ -39,19 +39,21 @@ function levelUp() {
 function checkAns(idx) {
     if (userSeq[idx] === gameSeq[idx]) {
         if (userSeq.length === gameSeq.length) {
-            setTimeout(levelUp, 1000);
+            setTimeout(levelUp, 800);
         }
     } else {
+
         if (highScore < level) {
             highScore = level;
         }
 
         h3.innerText = `High Score : ${highScore}`;
-        h2.innerHTML = `Game Over! Your score was ${level} <br> Presss any key to start.`;
+        h2.innerHTML =
+            `Game Over! Your score was ${level} <br> Press any key to start.`;
 
-        document.querySelector("body").style.backgroundColor = "red";
-        setTimeout(function () {
-            document.querySelector("body").style.backgroundColor = "aqua";
+        document.body.style.backgroundColor = "red";
+        setTimeout(() => {
+            document.body.style.backgroundColor = "aqua";
         }, 250);
 
         reset();
